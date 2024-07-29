d0cde2d672de084c3d0f2e4b133258988686781b
giuliocorradini <39673120+giuliocorradini@users.noreply.github.com>
2024-07-29 11:48:31 +0200

    Db structure for puzzles
    Migrations

--- AlgoBattles/settings.py
+++ AlgoBattles/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'puzzle.apps.PuzzleConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- puzzle/models.py
+++ puzzle/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+class Puzzle(models.Model):
+    title = models.CharField(max_length=150)
+
+    class DifficultyLevel(models.TextChoices):
+        EASY = "E", _("Easy")
+        MEDIUM = "M", _("Medium")
+        HARD = "H", _("Hard")
+
+    difficulty = models.CharField(
+        max_length=1,
+        choices=DifficultyLevel,
+        default=DifficultyLevel.EASY
+    )
+
+    description = models.TextField()
+
+    time_constraint = models.IntegerField() # in us
+    memory_constraint = models.IntegerField()
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    
+class PuzzleCategories(models.Model):
+    """
+    Table describing the categories of a Puzzle.
+    Each puzzle can have N different categories, that are defined in their table.
+    """
+
+    puzzle = models.ForeignKey(Puzzle, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    
+    constraints = [
+        models.UniqueConstraint(fields=["puzzle", "category"], name="categories_as_set")
+    ]
+
+class PuzzleTest(models.Model):
+    input = models.TextField()
+    output = models.TextField()
+    
+    is_private = models.BooleanField()
+
+    problem = models.ForeignKey(Puzzle, on_delete=models.CASCADE)
