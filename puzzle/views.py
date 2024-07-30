d722e66033a9a4cef387f88bd8404de4735be2a7
giuliocorradini <39673120+giuliocorradini@users.noreply.github.com>
2024-07-30 21:21:13 +0200

    Query puzzles by difficulty and categories

--- puzzle/views.py
+++ puzzle/views.py
@@ -1,10 +1,11 @@
-from rest_framework import generics, pagination, views
+from rest_framework import generics, pagination, views, status
 from rest_framework.response import Response
-from . import models, serializers
+from . import serializers
+from .models import Puzzle, Category
 
 class CategoryList(views.APIView):
     def get(self, request):
-        categories = models.Category.objects.values_list('name', flat=True)
+        categories = Category.objects.values_list('name', flat=True)
         return Response(list(categories))
 
 
@@ -14,6 +15,20 @@ class StandardResultsSetPagination(pagination.PageNumberPagination):
     max_page_size = 1000
 
 class PuzzleList(generics.ListAPIView):
-    queryset = models.Puzzle.objects.all()
     serializer_class = serializers.PuzzleListSerializer
     pagination_class = StandardResultsSetPagination
+
+    def get_queryset(self):
+        category = self.request.query_params.get("category", None)
+        difficulty = self.request.query_params.get("difficulty", None)
+
+        puzzles = Puzzle.objects.all()
+
+        if category is not None:
+            category = category.lower()
+            puzzles = puzzles.filter(categories__name=category)
+
+        if difficulty is not None:
+            puzzles = puzzles.filter(difficulty=difficulty)
+        
+        return puzzles
