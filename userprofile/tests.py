42486c29df724a7cb36d6283ee89ec9c049390c1
giuliocorradini <39673120+giuliocorradini@users.noreply.github.com>
2024-08-13 23:00:00 +0200

    Unit test for access control

--- userprofile/tests.py
+++ userprofile/tests.py
@@ -1,3 +1,45 @@
 from django.test import TestCase
+from django.test import Client
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APIClient
+import json
 
-# Create your tests here.
+class TestCORSOnUserFullInformation(TestCase):
+    """
+    Test UserFullInformationView with IsBrowseAuthenticated, which rejects unauthenticated requests
+    except for OPTIONS, which is used by the browser for CORS requests.
+    """
+
+    def setUp(self):
+        self.user = User.objects.create(username="test", password="aaa", email="test@algobattles.com")
+        self.token, _ = Token.objects.get_or_create(user=self.user)
+
+    def login(self):
+        client = Client()
+        response = client.post("/api/auth/login", {
+            "username": "test",
+            "password": "aaa"
+        }, content_type="application/json")
+        self.token = response.content.decode()
+
+    def test_options_has_permission(self):
+        client = Client()
+        response = client.options("/api/user")
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_unauth(self):
+        client = Client()
+        response = client.get("/api/user")
+
+        self.assertEqual(response.status_code, 401)
+
+    def test_get_auth(self):
+        # Create a fake 
+        client = APIClient()
+        client.credentials(HTTP_AUTHORIZATION=f"Token {self.token}")
+
+        response = client.get("/api/user")
+
+        self.assertEqual(response.status, 200)
