c985ad06d03bb22237fdca78e821134356ebca91
giuliocorradini <39673120+giuliocorradini@users.noreply.github.com>
2024-08-16 11:55:10 +0200

    User introspection tests

--- userprofile/tests.py
+++ userprofile/tests.py
@@ -15,14 +15,6 @@ class TestCORSOnUserFullInformation(TestCase):
         self.user = User.objects.create(username="test", password="aaa", email="test@algobattles.com")
         self.token, _ = Token.objects.get_or_create(user=self.user)
 
-    def login(self):
-        client = Client()
-        response = client.post("/api/auth/login", {
-            "username": "test",
-            "password": "aaa"
-        }, content_type="application/json")
-        self.token = response.content.decode()
-
     def test_options_has_permission(self):
         client = Client()
         response = client.options("/api/user")
@@ -36,10 +28,11 @@ class TestCORSOnUserFullInformation(TestCase):
         self.assertEqual(response.status_code, 401)
 
     def test_get_auth(self):
-        # Create a fake 
         client = APIClient()
-        client.credentials(HTTP_AUTHORIZATION=f"Token {self.token}")
+        client.credentials(HTTP_AUTHORIZATION=f"Token {self.token.key}")
 
         response = client.get("/api/user")
 
-        self.assertEqual(response.status, 200)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data.get("username"), "test")
+        self.assertTrue("password" not in response.data)
