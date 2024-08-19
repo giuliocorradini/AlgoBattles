578334f0903775c640ccae3e6de17227687b0a6b
giuliocorradini <39673120+giuliocorradini@users.noreply.github.com>
2024-08-19 21:09:37 +0200

    Serving static images

--- userprofile/models.py
+++ userprofile/models.py
@@ -5,4 +5,4 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     github = models.CharField(max_length=100, default="")
     linkedin = models.CharField(max_length=255, default="")
-    picture = models.ImageField(upload_to="propics")
+    picture = models.ImageField(upload_to="static/propics")
