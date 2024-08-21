9f72e646201457e04454fe1f5b564130da38d069
giuliocorradini <39673120+giuliocorradini@users.noreply.github.com>
2024-08-21 10:17:17 +0200

    Fixed password registration

--- restauth/serializers.py
+++ restauth/serializers.py
@@ -1,7 +1,14 @@
 from django.contrib.auth.models import User
-from rest_framework.serializers import ModelSerializer
+from django.contrib.auth.hashers import make_password
+from rest_framework.serializers import ModelSerializer, CharField
 
 class UserSerializer(ModelSerializer):
+    password = CharField(write_only=True, required=True)
+
     class Meta:
         model = User
         fields = ("username", "email", "password", "first_name", "last_name")
+
+    def create(self, validated_data):
+        validated_data['password'] = make_password(validated_data.get('password'))
+        return super().create(validated_data)
